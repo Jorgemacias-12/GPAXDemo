d7ce5f11ca885dcbef1a8a6ff38f875bdcc985a8
Jorge Macias <jorge.macias7905@alumnos.udg.mx>
2024-02-01 12:39:43 -0600

    update(project): some updated code :p

--- GPAXDemo/MainPage.xaml.cs
+++ GPAXDemo/MainPage.xaml.cs
@@ -5,14 +5,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace GPAXDemo
 {
     public partial class MainPage : ContentPage
     {
+        
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await ShowGeoLocation();
+
+            Btn_Record.Clicked += Btn_Record_Clicked;
+        }
+
+        private void Btn_Record_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Sexoo", "¿Cuando?", "Hoy");
+        }
+
+        private async Task ShowGeoLocation()
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location is null) return;
+
+                Lbl_Location.Text = $"Location: {location.Latitude} {location.Longitude} {location.Speed} {location.Timestamp}";
+            }
+            catch
+            {
+                
+            }
         }
     }
 }
